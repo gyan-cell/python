2ddafada1311405a22f183cb5c058838fe024d51
gyan-cell <jha07944@gmail.com>
2024-11-08 22:01:56 +0530

    Basic Intro To Python

--- hello.py
+++ hello.py
@@ -1,1 +1,30 @@
 print("Hello World")
+"We will be using Python 3.12.7 or later"
+
+"""
+Python is an easy to learn, powerful programming language.
+It has efficient high-level data structures and a simple but effective approach to object-oriented programming.
+Python’s elegant syntax and dynamic typing, together with its interpreted nature, make it an ideal language
+for scripting and rapid application development in many areas on most platforms."""
+"""
+Python is simple to use, but it is a real programming language, offering much more structure and support
+for large programs than shell scripts or batch files can offer. 
+On the other hand, Python also offers much more error checking than C, and, being a very-high-level language,
+it has high-level data types built in, such as flexible arrays and dictionaries. Because of its more general data types Python
+is applicable to a much larger problem domain than Awk or even Perl, yet many things are at least as easy in Python as in those languages.
+Python allows you to split your program into modules that can be reused in other Python programs.
+It comes with a large collection of standard modules that you can use as the basis of your programs
+— or as examples to start learning to program in Python. Some of these modules provide things like file I/O, system calls,
+sockets, and even interfaces to graphical user interface toolkits like Tk.
+"""
+
+
+"""
+Working With Python Interpreter :  The Python interpreter is usually installed as /usr/local/bin/python3.13 on those
+machines where it is available; putting /usr/local/bin in your Unix shell’s search path makes it possible to start it by typing the command: 
+python3.13
+When commands are read from a tty, the interpreter is said to be in interactive mode. 
+In this mode it prompts for the next command with the primary prompt, usually three greater-than signs (>>>);
+for continuation lines it prompts with the secondary prompt, by default three dots (...).
+The interpreter prints a welcome message stating its version number and a copyright notice before printing the first prompt:
+"""
