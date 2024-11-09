c7fe33056b6225b62dd693e62240bd1e7738b415
gyan-cell <jha07944@gmail.com>
2024-11-09 21:19:36 +0530

    Strings

--- basics.py
+++ basics.py
@@ -2,6 +2,7 @@
 
 
 a=876
+#here a and b are the variables and equato sign is used to assign values
 b=10
 # This will add two numbers
 print(a+b)
@@ -19,3 +20,36 @@ print(a//b)
 print(a**b)
 
 
+"""Python can manipulate text (represented by type str, so-called “strings”) as well as numbers. 
+This includes characters “!”, words “rabbit”, names “Paris”, sentences “Got your back.”, etc. “Yay! :)”.
+They can be enclosed in single quotes ('...') or double quotes ("...") with the same result ."""
+
+
+string  = "Yay! :) , 'Yes', they said.'"
+print("""\
+
+Usage: thingy [OPTIONS]
+
+     -h                        Display this usage message
+
+     -H hostname               Hostname to connect to
+
+""")
+
+
+print(string)
+"Strings can be concatenated (glued together) with the + operator, and repeated with *:"
+print("Hello" + "World")
+print("Hello" * 3)
+print(3 * 'un' + 'ium')
+
+
+"""Strings can be indexed (subscripted), with the first character having index 0. 
+There is no separate character type; a character is simply a string of size one:"""
+
+word = 'Python'
+print(word[0])  # print 'P'
+print(word[5])  # print 'n'
+print(word[-1])  # print 'n'
+print(word[-2])  # print 'o'
+print(word[-6])  # print 'P'
