c3804dba19fb31498986de54b1aceef37e508c59
gyan-cell <jha07944@gmail.com>
2024-11-09 21:01:05 +0530

    basic numbers operation

---
+++ basics.py
@@ -0,0 +1,21 @@
+"""Numbers In Python"""
+
+
+a=876
+b=10
+# This will add two numbers
+print(a+b)
+# This will subtract two Numbers
+print(a-b)
+# This will multiply two Numbers
+print(a*b)
+# This will divide two Numbers and returns a floating point number which happens to be the quotient
+print(a/b)
+# This will return the remainder
+print(a%b)
+# This will return the integer division
+print(a//b)
+# This will return the power
+print(a**b)
+
+
