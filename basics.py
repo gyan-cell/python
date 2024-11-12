1339e46957afc9fb8815882c4bc8bf55aa4ccc7f
gyan-cell <jha07944@gmail.com>
2024-11-12 12:32:35 +0530

    Basics

--- basics.py
+++ basics.py
@@ -1,21 +1,20 @@
 """Numbers In Python"""
 
-
-a=876
-#here a and b are the variables and equato sign is used to assign values
-b=10
+a = 876
+# here a and b are the variables and equato sign is used to assign values
+b = 10
 # This will add two numbers
-print(a+b)
+print(a + b)
 # This will subtract two Numbers
-print(a-b)
+print(a - b)
 # This will multiply two Numbers
-print(a*b)
+print(a * b)
 # This will divide two Numbers and returns a floating point number which happens to be the quotient
-print(a/b)
+print(a / b)
 # This will return the remainder
-print(a%b)
+print(a % b)
 # This will return the integer division
-print(a//b)
+print(a // b)
 # This will return the power
 print(a**b)
 
@@ -25,8 +24,9 @@ This includes characters “!”, words “rabbit”, names “Paris”, sentenc
 They can be enclosed in single quotes ('...') or double quotes ("...") with the same result ."""
 
 
-string  = "Yay! :) , 'Yes', they said.'"
-print("""\
+string = "Yay! :) , 'Yes', they said.'"
+print(
+    """\
 
 Usage: thingy [OPTIONS]
 
@@ -34,22 +34,40 @@ Usage: thingy [OPTIONS]
 
      -H hostname               Hostname to connect to
 
-""")
+"""
+)
 
 
 print(string)
 "Strings can be concatenated (glued together) with the + operator, and repeated with *:"
 print("Hello" + "World")
 print("Hello" * 3)
-print(3 * 'un' + 'ium')
+print(3 * "un" + "ium")
 
 
 """Strings can be indexed (subscripted), with the first character having index 0. 
 There is no separate character type; a character is simply a string of size one:"""
 
-word = 'Python'
+word = "Python"
 print(word[0])  # print 'P'
 print(word[5])  # print 'n'
 print(word[-1])  # print 'n'
 print(word[-2])  # print 'o'
 print(word[-6])  # print 'P'
+
+
+"""
+Lists In Python 
+List is a collection which is ordered and changeable. Allows duplicate members.
+python knows a number of compound data types, used to group together other values. The most versatile 
+is the list, which can be written as a list of comma-separated values (items) between square brackets.
+Lists might contain items of different types, but usually the items all have the same type.
+
+"""
+
+qans = [1, 2, 3, 4, 5]
+print(qans[0])
+print(qans[1])
+print(qans[2])
+print(qans[3])
+print(qans[4])
